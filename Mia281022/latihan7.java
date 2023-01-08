4b7db5a2e53518eb86cc7bd42656022682535a71
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-01-08 15:58:50 +0700

    Create latihan7.java

---
+++ Mia281022/latihan7.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia281022;
+
+/**
+ *
+ * @author user
+ */
+public class latihan7 {
+    
+     public static void main(String[] args) {
+        double a = Double.parseDouble(args[2]);
+        double b = Double.parseDouble(args[3]);
+        
+        double Sum,difference,product,quotient;
+        
+        Sum = a + b;
+        difference = a - b;
+        product = a * b;
+        quotient = a / b;
+        
+        System.out.println("Sum = "+Sum);        
+        System.out.println("Difference = "+difference);        
+        System.out.println("Product = "+product);      
+        System.out.println("quetient = "+quotient);
+    }
+}
+    
+
