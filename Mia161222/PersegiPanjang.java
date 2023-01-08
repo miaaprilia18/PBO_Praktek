39fed3dcf58b1049abfa4af8ce192b31a52a887a
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-01-08 15:57:39 +0700

    Create PersegiPanjang.java

---
+++ Mia161222/PersegiPanjang.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia161222;
+
+/**
+ *
+ * @author user
+ */
+public class PersegiPanjang {
+    
+        
+    public PersegiPanjang() {   
+    }
+    
+    private double x1;
+    private double y1;
+  
+    public PersegiPanjang(double x1,  double y1){
+    this.x1 = x1;
+    this.y1 = y1;
+        
+    
+}
+   
+ 
+    
+}
