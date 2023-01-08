78aa0f3363855c7b43f15b779ff99ddb084eb095
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-01-08 15:58:39 +0700

    Create latihan2.java

---
+++ Mia281022/latihan2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia281022;
+import java.io.*;
+
+/**
+ *
+ * @author user
+ */
+public class latihan2 {
+    
+     public static void main (String [] args){
+  
+         
+         // for loop array
+       String days[]={"Monday","Tuesday","wednesday","Thursdya","friday","saturday","sunday"};
+       
+       for(int i=0;i< days.length; i++){
+       
+            System.out.println(days[i]);
+           
+       }
+    }
+         
+}
+    
+
