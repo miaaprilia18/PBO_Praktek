3c5b40840c714814da059e5a57baf933e309449d
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-01-08 15:56:45 +0700

    Create latihan1Example.java

---
+++ Mia02Des2022/latihan1Example.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia02Des2022;
+
+
+
+/**
+ *
+ * @author user
+ */
+public class latihan1Example {
+    public static void main (String[] args) {
+        latihan1Record student1 = new latihan1Record("Mia","Padang","miaaprilia184@gmail.com","0895602340547");
+      
+
+        //
+        
+        System.out.println("Nama                :"+student1.getName());
+        System.out.println("Alamat              :"+student1.getAddress());
+        System.out.println("Email               :"+student1.getEmail());
+        System.out.println("No Tel              :"+student1.getNo());
+       
+
+    }
+}
