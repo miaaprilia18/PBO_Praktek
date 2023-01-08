a1b531c9347617a2816d72ef756b7d858293be97
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-01-08 15:58:05 +0700

    Create latihan10.java

---
+++ Mia230922/latihan10.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia230922;
+
+/**
+ *
+ * @author user
+ */
+public class latihan10 {
+     public static void main( String[] args ){ 
+     
+      System.out.println("nummber =10\n"+"letter =a \n"+"result=true\n"+" str =hello  ");
+     }
+}
