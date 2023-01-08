2173ce9243aa22c2910e265f9a7e17a988a42dc0
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-01-08 15:57:07 +0700

    Create latihan4.java

---
+++ Mia071022/latihan4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia071022;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author user
+ */
+public class latihan4 {
+    
+      
+    
+    
+       
+
+    
+}
