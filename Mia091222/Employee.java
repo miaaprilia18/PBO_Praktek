1693d90fcf8629aaa4201b17febf29ffd7844984
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-01-08 15:57:16 +0700

    Create Employee.java

---
+++ Mia091222/Employee.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia091222;
+
+/**
+ *
+ * @author user
+ */
+public class Employee  extends person{
+    
+    public String getName(){
+    System.out.println("Employee Name:" + name);
+    return name;
+    
+    }
+}
