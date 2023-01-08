3e53c4dd93857b4857b530c3e4f9df7fc135cde5
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-01-08 15:56:52 +0700

    Create StudentRecord1.java

---
+++ Mia02Des2022/StudentRecord1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia02Des2022;
+
+/**
+ *
+ * @author user
+ */
+public class StudentRecord1 {
+    
+        
+  
+}
