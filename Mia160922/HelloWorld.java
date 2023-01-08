f42581cad81254bd4c09856dd76442c22d054f09
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-01-08 15:57:26 +0700

    Create HelloWorld.java

---
+++ Mia160922/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia160922;
+
+/**
+ *
+ * @author user
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Mia160922.HelloWorld "+" MI2C "+" pemograman berbasis objek ()");
+        System.out.println("Nama\t Mia Aprilia\n "+"nobp\t 2101091007\n "+"prodi\t Manajemen Informatika\n "+"jurusan\t Tekologi Informasi \n "+"alamat\t Jln By Pass KM7()");
+    }
+}
