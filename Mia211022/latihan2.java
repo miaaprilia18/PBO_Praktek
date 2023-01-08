e16e41797accf297dd02454b097a7bd69f686d06
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-01-08 15:57:45 +0700

    Create latihan2.java

---
+++ Mia211022/latihan2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia211022;
+
+/**
+ *
+ * @author user
+ */
+public class latihan2 {
+    
+    public static void main (String [] arg) {
+       int x=0;
+       do 
+       {
+           System.out.println("Mia");
+           x++;
+       }while (x <= 100);
+    }
+}
