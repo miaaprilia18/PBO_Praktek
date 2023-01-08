5593889a791a04eae966a535ad278851ccc885b3
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-01-08 15:57:43 +0700

    Create latihan1.java

---
+++ Mia211022/latihan1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia211022;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author user
+ */
+public class latihan1 {
+    
+    public static void main (String [] arg) {
+       int x=0;
+       while (x <= 100)
+       {
+           System.out.println("Mia");
+           x++;
+       }
+    }
+}
