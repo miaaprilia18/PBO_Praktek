bd3a9abc61e4907880a5bb76e3b5883a263b3798
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2023-01-08 15:57:28 +0700

    Create InterfaceExample.java

---
+++ Mia161222/InterfaceExample.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mia161222;
+
+/**
+ *
+ * @author user
+ */
+public class InterfaceExample {
+    public static void main (String [] args){
+        Relation myInteger = new MyInteger ();
+        Relation myDouble = new MyDouble ();
+        boolean hasilInt = myInteger.isGreater(5,6);
+        boolean hasilDbl = myDouble.isGreater(6.4,2.2);
+   
+        
+        System.out.println("Hasil Integer  : "     +hasilInt);
+        System.out.println("Hasil Double   : "      +hasilDbl);
+        
+        Relation myline  = new Line ();
+        Line line1 = new Line (2,5,2,7);
+        Line line2 = new Line (2,5,2,2);
+        boolean lineA_lineB = myline.isGreater(line1,line2);
+        
+         System.out.println("Line A > line B : "      +lineA_lineB);
+        
+        
+    }
+    
+}
